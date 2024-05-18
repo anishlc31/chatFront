0ad6317df95bd93d2c8162e0bc568e72ba562e5a
Anish Lamichhane <codingonly31@gmail.com>
2024-05-18 21:59:23 +0545

    not pefectly work cause of backend

--- src/app/public/user.service.ts
+++ src/app/public/user.service.ts
@@ -4,6 +4,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { Observable, throwError } from 'rxjs';
 import { UserI } from 'src/app/model/user.interface';
 import { catchError, tap } from 'rxjs/operators';
+import { LoginResponseI } from '../model/login-response.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -13,11 +14,11 @@ export class UserService {
   constructor(private http: HttpClient, private snackbar: MatSnackBar) { }
 
  
-  private apiUrl = 'http://localhost:3000/api/user/singup'; // Adjust the port if necessary
+  private apiUrl = 'http://localhost:3000/api/user'; // Adjust the port if necessary
 
   signup(user: UserI): Observable<UserI> {
     console.log(user.Username)
-    return this.http.post<UserI>(`${this.apiUrl}`, user).pipe(
+    return this.http.post<UserI>(`${this.apiUrl}/singup`, user).pipe(
       tap((createdUser: UserI) => this.snackbar.open(`User ${createdUser.Username} created successfully`, 'Close', {
         duration: 2000, horizontalPosition: 'right', verticalPosition: 'top'
       })),
@@ -30,7 +31,15 @@ export class UserService {
     )
   }
 
-  // signup(user: any): Observable<any> {
-  //   return this.http.post(`${this.apiUrl}`, user);  // Ensure the URL matches your backend endpoint
-  // }
+  
+
+  login(user: UserI): Observable<LoginResponseI> {
+    return this.http.post<LoginResponseI>(`${this.apiUrl}/login`, user).pipe(
+      tap((res: LoginResponseI) => localStorage.setItem('nestjs_chat_app', res.access_token)),
+      tap(() => this.snackbar.open('Login Successfull', 'Close', {
+        duration: 2000, horizontalPosition: 'right', verticalPosition: 'top'
+      }))
+    );
+  }
+
 }
