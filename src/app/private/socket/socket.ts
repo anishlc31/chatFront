a0ce2ea26cb286f3392be9d75ac7c14502ae622b
Anish Lamichhane <codingonly31@gmail.com>
2024-06-12 23:38:26 +0545

    custom soket added

--- src/app/private/socket/socket.ts
+++ src/app/private/socket/socket.ts
@@ -2,21 +2,21 @@ import { Injectable } from '@angular/core';
 import { Socket, SocketIoConfig } from 'ngx-socket-io';
 import { tokenGetter } from 'src/app/app.module';
 
-
-const config: SocketIoConfig = {
-  url: 'http://localhost:3000', options: {
-    extraHeaders: {
-      Authorization: tokenGetter()
+const getSocketConfig = (): SocketIoConfig => {
+  const token = tokenGetter();
+  return {
+    url: 'http://localhost:3000',
+    options: {
+      extraHeaders: {
+        Authorization: token ? token : '' // Ensure Authorization is always a string
+      }
     }
-  }
+  };
 };
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class CustomSocket extends Socket {
   constructor() {
-    super(config)
+    super(getSocketConfig());
   }
 }
-
-
-// fuck git 
