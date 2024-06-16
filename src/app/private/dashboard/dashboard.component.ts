00345f73fbadd68f71bde97183e43f12f2bb8332
Anish Lamichhane <codingonly31@gmail.com>
2024-06-16 13:46:40 +0545

    simply frontend

--- src/app/private/dashboard/dashboard.component.ts
+++ src/app/private/dashboard/dashboard.component.ts
@@ -1,4 +1,26 @@
+// import { Component, OnInit } from '@angular/core';
+// import { ChatServiceService } from '../chat-service.service';
+
+// @Component({
+//   selector: 'app-dashboard',
+//   templateUrl: './dashboard.component.html',
+//   styleUrls: ['./dashboard.component.scss']
+// })
+// export class DashboardComponent {
+
+//  rooms$ = this.chatService.getMyRoom() 
+
+
+//   constructor( private chatService : ChatServiceService){}
+
+//   OnInit(){
+//     this.chatService.createRoom()
+//   }
+// }
+
+
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ChatServiceService } from '../chat-service.service';
 
 @Component({
@@ -6,15 +28,32 @@ import { ChatServiceService } from '../chat-service.service';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  rooms: any[] = [];
+  roomForm: FormGroup;
 
-//  rooms$ = this.chatService.getMyRoom() 
+  constructor(private chatService: ChatServiceService, private fb: FormBuilder) {
+    this.roomForm = this.fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required]
+    });
+  }
 
-title = this.chatService.getMessage()
+  ngOnInit(): void {
+    this.chatService.getRooms().subscribe((rooms: any) => {
+      this.rooms = rooms.items;
+    });
 
-  constructor( private chatService : ChatServiceService){}
+    this.chatService.onError().subscribe((error: any) => {
+      console.error('Error:', error);
+    });
+  }
 
-  OnInit(){
-  //  this.chatService.createRoom()
+  createRoom() {
+    if (this.roomForm.valid) {
+      this.chatService.createRoom(this.roomForm.value);
+      this.roomForm.reset();
+    }
   }
 }
+
