69edcb1e849fee5fc19a8623fd0575e7580c3768
Anish Lamichhane <codingonly31@gmail.com>
2024-06-23 22:27:10 +0545

    first day

---
+++ src/app/private/chat.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+import { CustomSocket } from './socket/socket';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+
+  constructor(private socket : CustomSocket , private http : HttpClient) {
+  }
+
+
+  private apiUrl = 'http://localhost:3000';
+
+  joinRoom(userId: string) {
+    this.socket.emit('joinRoom', userId);
+  }
+
+  sendMessage(message: { senderId: string; receiverId: string; content: string }) {
+    this.socket.emit('sendMessage', message);
+  }
+
+  receiveMessage(callback: (message: any) => void) {
+    this.socket.on('receiveMessage', callback);
+  }
+
+  
+  saveMessage(message: { senderId: string; receiverId: string; content: string }): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/messages`, message);
+  }
+
+  getMessages(user1Id: string, user2Id: string): Observable<any[]> {
+    return this.socket.emit('getMessages', { user1Id, user2Id });
+  }
+
+}
+
+
