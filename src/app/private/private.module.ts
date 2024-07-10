6df92a20c09a0c01c3542e871572c00de78ebcfb
Anish Lamichhane <codingonly31@gmail.com>
2024-07-10 23:53:14 +0545

    adding user list

--- src/app/private/private.module.ts
+++ src/app/private/private.module.ts
@@ -4,15 +4,26 @@ import { MatLineModule } from '@angular/material/core';
 import { PrivateRoutingModule } from './private-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import {MatListModule} from '@angular/material/list';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatIconModule} from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChatComponent } from './chat/chat.component';
+import { UserlistComponent } from './userlist/userlist.component';
 
+//import {MatChipsModule} from '@angular/material/chips';
 
 @NgModule({
   declarations: [
     DashboardComponent,
+    ChatComponent,
+    UserlistComponent,
     
   ],
   imports: [
@@ -23,8 +34,13 @@ import { MatPaginatorModule } from '@angular/material/paginator';
     ReactiveFormsModule,
     MatCardModule,
     MatButtonModule,
-    MatPaginatorModule
-    
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatChipsModule,
+    MatAutocompleteModule,
+    MatIconModule,
+    FormsModule    
   ]
 })
 export class PrivateModule { }
