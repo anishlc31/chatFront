cca7416ed4d8858ddd0e418afdc43b10d6cfcaff
Anish Lamichhane <codingonly31@gmail.com>
2024-07-13 21:40:23 +0545

    just to streak

--- src/app/private/userlist/userlist.component.ts
+++ src/app/private/userlist/userlist.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Conversation, UserI } from 'src/app/model/user.interface';
+import { UserlistService } from './../userlist.service';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChatService } from '../chat.service';
+import { UserService } from 'src/app/public/user.service';
 
 @Component({
   selector: 'app-userlist',
@@ -6,5 +10,96 @@ import { Component } from '@angular/core';
   styleUrls: ['./userlist.component.scss']
 })
 export class UserlistComponent {
+
+  showSearchInput: boolean = false;
+  @Input() selectedUser: UserI | null = null;
+  @Input()  users: UserI[] = [];
+  @Input()  typingStatus: { [key: string]: boolean } = {}; 
+  currentUser: UserI | null = null;
+  conversation:Conversation[]=[]
+  unseenMessages: { [key: string]: number } = {}; 
+  updateChatAtMap: { [key: string]: Date } = {};
+
+
+  constructor(private userlistService: UserlistService,  
+     private webSocketService: ChatService,
+    private userService: UserService) {}
+
+  ngOnInit() {
+
+    //for search button 
+    this.userlistService.searchClicked$.subscribe(() => {
+      this.showSearchInput = !this.showSearchInput;
+    });
+
+    this.currentUser = this.userService.getCurrentUser();
+
+  
+    if (this.currentUser) {
+
+      //fething and sorting user list 
+      this.userService.getAllUsers().subscribe((data) => {
+        this.users = data.filter(user => user.id !== this.currentUser!.id);
+        this.fetchAndSortConversations();
+
+      });
+  
+ 
+  //count unseen msg 
+      this.webSocketService.getUnseenMessageCounts((counts: any) => {
+        this.unseenMessages = counts;
+      });
+  
+      this.webSocketService.requestUnseenMessageCounts(this.currentUser.id);
+
+   
+
+      // Typing status
+      this.webSocketService.onUserTyping((data) => {
+        this.typingStatus[data.senderId] = data.typing;
+      });
+
+      //for update list 
+
+   this.webSocketService.getConversationUpdate((conversation) => {
+    const userId = conversation.user1Id === this.currentUser!.id ? conversation.user2Id : conversation.user1Id;
+    this.updateChatAtMap[userId] = conversation.updateChatAt;
+    this.sortUsersByUpdateChatAt();
+  });
+     
+    }
+}
+
+
+
+
+  fetchAndSortConversations(): void {
+    if (this.currentUser) {
+      this.webSocketService.getConversations(this.currentUser.id).subscribe((conversations) => {
+        conversations.forEach(conversation => {
+          const userId = conversation.user1Id === this.currentUser!.id ? conversation.user2Id : conversation.user1Id;
+          this.updateChatAtMap[userId] = conversation.updateChatAt;
+        });
+        this.sortUsersByUpdateChatAt();
+      });
+    }
+  }
+
+
+  sortUsersByUpdateChatAt(): void {
+    this.users.sort((a, b) => {
+      const aUpdate = this.updateChatAtMap[a.id!] || new Date(0);
+      const bUpdate = this.updateChatAtMap[b.id!] || new Date(0);
+      return new Date(bUpdate).getTime() - new Date(aUpdate).getTime();
+    });
+  }
+
+
+  
+
+  onUserSelect(users : UserI) {
+    
+    }
+    
 
 }
