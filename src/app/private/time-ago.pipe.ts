ea81e85c1fb727feaa47f9873798fee54817efca
Anish Lamichhane <codingonly31@gmail.com>
2024-07-14 13:22:57 +0545

    adding time pipe

---
+++ src/app/private/time-ago.pipe.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'timeAgo'
+})
+export class TimeAgoPipe implements PipeTransform {
+
+  transform(value: Date): string {
+    const now = new Date();
+    const diffMs = now.getTime() - new Date(value).getTime();
+    const diffSeconds = Math.floor(diffMs / 1000);
+    const diffMinutes = Math.floor(diffSeconds / 60);
+    const diffHours = Math.floor(diffMinutes / 60);
+    const diffDays = Math.floor(diffHours / 24);
+
+    if (diffMinutes < 60) {
+      return `${diffMinutes} min `;
+    } else if (diffHours < 24) {
+      return `${diffHours} hours `;
+    } else {
+      return new Date(value).toLocaleDateString();
+    }
+  }
+}
