7f0cb5565f86b0fb9efd9c667c09a1aeb40092e2
Anish Lamichhane <codingonly31@gmail.com>
2024-08-05 20:42:55 +0545

    adding adfriend

---
+++ src/app/private/addfriend/addfriend.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addfriend',
+  templateUrl: './addfriend.component.html',
+  styleUrls: ['./addfriend.component.scss']
+})
+export class AddfriendComponent {
+
+}
---
+++ src/app/private/addfriend/addfriend.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddfriendComponent } from './addfriend.component';
+
+
+
+@NgModule({
+  declarations: [
+    AddfriendComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AddfriendModule { }
