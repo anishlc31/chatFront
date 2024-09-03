3e0bd6594c17e8b53df43f0755c2e9c4e197798c
Anish Lamichhane <codingonly31@gmail.com>
2024-09-03 21:27:00 +0545

    ading friendlist

---
+++ src/app/private/friendlist/friendlist.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FriendlistComponent } from './friendlist.component';
+
+
+
+@NgModule({
+  declarations: [
+    FriendlistComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FriendlistModule { }
